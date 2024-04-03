0a680fb49482d006546b8b30160caac55e3ee15a
Ahmedhismail <ahmedhismail@berkeley.edu>
2024-04-03 02:28:28 -0700

    figured out how to create a route, with parameters, and how to access querystring (validating with postman)

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-async function searchHandler() {
-  const resp = await fetch(
-    "https://itunes.apple.com/search?term=Joe+Rogan&limit=2"
-  );
-  console.log(await resp.json());
-}
-searchHandler();
-// const resp = ;
+const fastify = require("fastify")({ logger: true });
+
+const getOptions = {
+  schema: {
+    querystring: {
+      RSP: { type: "string" },
+    },
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          response: { type: "string" },
+        },
+      },
+    },
+  },
+};
+
+fastify.get("/search", getOptions, (request, reply) => {
+  reply.status(200);
+  reply.send({ hello: "world" });
+  console.log(`\n ${request.query.RSP} \n`);
+  //   console.log(reply);
+});
+
+// async function searchHandler() {
+//   const resp = await fetch(
+//     "https://itunes.apple.com/search?term=Joe+Rogan&limit=2"
+//   );
+//   return await resp.json();
+// }
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log("now listening at http://localhost:3000/");
+  // Server is now listening on ${address}
+});
