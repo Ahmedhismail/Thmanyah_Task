fe5fcb0ca45ecdbfd6922f600e098c23a728a094
Ahmedhismail <ahmedhismail@berkeley.edu>
2024-04-03 03:20:34 -0700

    working on sending response payload instead of logging to console

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ const getOptions = {
 };
 
 fastify.get("/search", getOptions, async (request, reply) => {
+  ret = await searchHandler(request.query.RSP);
   reply.status(200);
-  reply.send({ hello: "world" });
+  reply.send({ response: "worked!" });
   // console.log(`\n ${request.query.RSP} \n`);
 
-  console.log(
-    `\n ${JSON.stringify(await searchHandler(request.query.RSP), null, 2)} \n`
-  );
+  console.log(await ret.json());
 });
 
 async function searchHandler(searchQeury) {
   const resp = await fetch(
-    `https://itunes.apple.com/search?term=${searchQeury}&limit=2` // dont forget to modify limit when finalizing
+    `https://itunes.apple.com/search?term=${searchQeury}&limit=5` // dont forget to modify limit when finalizing
   );
-  return await resp.json();
+
+  return resp;
 }
 
 fastify.listen({ port: 3000 }, function (err, address) {
@@ -39,5 +39,4 @@ fastify.listen({ port: 3000 }, function (err, address) {
     process.exit(1);
   }
   console.log("now listening at http://localhost:3000/");
-  // Server is now listening on ${address}
 });
